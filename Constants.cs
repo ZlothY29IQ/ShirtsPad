ae431c6a92a52b2b19a6767fb82f956c93f4a490
ZlothY29IQ <211057194+ZlothY29IQ@users.noreply.github.com>
2025-11-01 13:31:24 +0000

    Update Version

--- Constants.cs
+++ Constants.cs
@@ -8,5 +8,5 @@ internal class Constants
     public const string Description =
             "Shirts Pad is a mod for [Gorilla Shirts ©] (made by Developer9998) which allows you to change your shirts from anywhere. Made in the style of [Holdable Pad ©]";
 
-    public const string Version = "1.0.2";
+    public const string Version = "1.0.3";
 }
